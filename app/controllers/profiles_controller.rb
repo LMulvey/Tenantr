24ad0414584d4831be2bbc0d1d71d00945bb5655
Lee Mulvey <lmulvey@me.com>
2016-01-30 04:57:41 +0000

    Profiles controller.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,10 @@
 class ProfilesController < ApplicationController
+    def new
+        # Form show up on the page that allows the user to fill out info for their profile.
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile
+    end
+    
     def create
     
     end
