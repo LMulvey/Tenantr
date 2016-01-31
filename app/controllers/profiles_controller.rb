1176e3d4ef54a432c6c3c97f5b67772688f4faf9
Lee Mulvey <lmulvey@me.com>
2016-01-31 18:17:04 +0000

    Profile show.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,6 +14,7 @@ class ProfilesController < ApplicationController
            redirect_to user_path( params[:user_id] ) 
         else
             flash[:danger] = "Error saving profile."
+            render action: :new
         end
     end
     
