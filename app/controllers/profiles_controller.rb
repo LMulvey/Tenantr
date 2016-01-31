7c412cd93ebea5bf96c0be7125a92e49db932d94
Lee Mulvey <lmulvey@me.com>
2016-01-31 19:20:49 +0000

    Redesigned dashboard for users who are logged in.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -19,11 +19,21 @@ class ProfilesController < ApplicationController
     end
     
     def edit
-        
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile()
     end
     
-    def show
+    def update
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile(profile_params)
         
+        if @profile.save
+            flash[:success] = "Profile updated!"
+           redirect_to user_path( params[:user_id] ) 
+        else
+            flash[:danger] = "Error saving profile."
+            render action: :new
+        end
     end
     
     private
