3f0ac0ac15cae5ce0b4cb406ecca3c82232db584
Lee Mulvey <lmulvey@me.com>
2016-02-01 03:31:19 +0000

    Added before filters to user and profile controllers for authentication.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_current_user
+    
     def new
         @user = User.find( params[:user_id] )
         @profile = Profile.new
@@ -9,8 +12,8 @@ class ProfilesController < ApplicationController
         @profile = @user.build_profile(profile_params)
         
         if @profile.save
-            flash[:success] = "Profile updated!"
-           redirect_to user_path( params[:user_id] ) 
+            flash[:success] = "Profile successfully created!"
+            redirect_to user_path( params[:user_id] ) 
         else
             flash[:danger] = "Error saving profile."
             render action: :new
@@ -23,11 +26,24 @@ class ProfilesController < ApplicationController
     end
     
     def update
-
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+        if @profile.update_attributes(profile_params)
+            flash[:success] = "Profile successfully updated!"
+            redirect_to user_path( params[:user_id] )
+        else
+            flash[:danger] = "Error saving profile."
+            render action: :edit
+        end
     end
     
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
+        
+        def only_current_user
+            @user = User.find( params[:user_id] )
+            redirect_to(root_url) unless @user == current_user
         end
 end
